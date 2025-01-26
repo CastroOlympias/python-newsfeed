51026b5862cf5fe8c51115b22bdfa11b6af80ef3
Castro <blackcarrera@msn.com>
2025-01-26 09:22:31 -0700

    added database connection to the app init py, server is running on 127 from this point

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from app.routes import home, dashboard
 from flask import Flask
+from app.db import init_db
 
 def create_app(test_config=None):
 
@@ -25,7 +26,7 @@ def create_app(test_config=None):
   
   app.register_blueprint(home)
   app.register_blueprint(dashboard)
-
+  init_db()
   return app
 
   
