e087fd3fe10499fb25cab1b37b03f8369620c438
Castro <blackcarrera@msn.com>
2025-01-26 11:11:20 -0700

    using single post query has no problem but he get all posts is still causing an inter server error due to attributeEorror sessionmaker object has no attribute query

--- app/routes/home.py
+++ app/routes/home.py
@@ -8,12 +8,13 @@ bp = Blueprint('home', __name__, url_prefix='/')
 def index():
   # get all posts
   db = get_db()
-  posts = db.query(Post).order_by(Post.created_at.desc()).all()
+  posts = db.query(Post).order_by(Post.created_at.desc()).all() # getting errors with this and
+  # Error: posts = db.query(Post).order_by(Post.created_at.desc()).all() AttributeError: 'sessionmaker' object has no attribute 'query'
   
 
   return render_template(
     'homepage.html',
-    posts=posts
+    posts=posts # and this one, error: 
   )
 
 @bp.route('/login')
@@ -25,5 +26,17 @@ def login():
 @bp.route('/post/<id>')
 
 def single(id):
+  
+  # get single post by id
+  db = get_db()
+  
+  post = db.query(Post).filter(Post.id == id).one()
+  
+  # render single post template
 
-  return render_template('single-post.html')
+  return render_template(
+
+    'single-post.html',
+
+    post=post
+  )
