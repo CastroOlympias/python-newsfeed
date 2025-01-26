f5bc2ff4154eeed918708d83abe5a61fd52ae2e9
Castro <blackcarrera@msn.com>
2025-01-25 19:37:11 -0700

    now enctrypting passwords in sql database

--- app/models/User.py
+++ app/models/User.py
@@ -1,5 +1,9 @@
 from app.db import Base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import validates
+import bcrypt
+
+salt = bcrypt.gensalt()
 
 class User(Base):
 
@@ -12,3 +16,22 @@ class User(Base):
     email = Column(String(50), nullable=False, unique=True)
 
     password = Column(String(100), nullable=False)
+    
+    @validates('email')
+    
+    def validate_email(self,key,email):
+        
+        # mke sure email address contains @ character
+        
+        assert '@' in email
+        
+        return email
+    
+    @validates('password')
+    
+    def validate_password(self, key, password):
+        assert len(password) > 4
+        
+        # encrypt password
+        
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
